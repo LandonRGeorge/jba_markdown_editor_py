d7b52d734271b4e12c95cf16d7c8440867fae7c7
LandonRGeorge <landon.robert.george@gmail.com>
2022-12-12 12:17:06 -0600

    finish stage 5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -14,18 +14,13 @@ def get_text() -> str:
     return input("Text: ")
 
 
-class InvalidHeaderRange(Exception):
-    pass
-
-
 def make_header() -> str:
     while True:
         n = input("Level: ")
         try:
             n = int(n)
-            if n < 1 or n > 6:
-                raise InvalidHeaderRange
-        except (ValueError, InvalidHeaderRange):
+            assert n >= 1 or n <= 6
+        except (ValueError, AssertionError):
             print("The level should be within the range of 1 to 6")
             continue
         break
@@ -46,6 +41,35 @@ def make_new_line() -> str:
     return "\n"
 
 
+def get_list(ordered=False) -> str:
+    while True:
+        nbr_rows = input("Number of rows: ")
+        try:
+            nbr_rows = int(nbr_rows)
+            assert nbr_rows > 0
+        except (ValueError, AssertionError):
+            print("The number of rows should be greater than zero")
+            continue
+        break
+    rows = []
+    for i in range(1, nbr_rows + 1):
+        row = input(f"Row #{i}: ")
+        if ordered:
+            row = f"{i}. {row}"
+        else:
+            row = f"* {row}"
+        rows.append(row)
+    return "\n".join(rows) + "\n"
+
+
+def make_ordered_list() -> str:
+    return get_list(ordered=True)
+
+
+def make_unordered_list() -> str:
+    return get_list(ordered=False)
+
+
 def print_help() -> None:
     msg = (
         f'Available formatters: {" ".join(list(formatters))}'
@@ -62,6 +86,8 @@ formatters = {
     "link": make_link,
     "inline-code": make_inline_code,
     "new-line": make_new_line,
+    "ordered-list": make_ordered_list,
+    "unordered-list": make_unordered_list,
 }
 
 texts = ""
@@ -71,6 +97,8 @@ while True:
         print_help()
         continue
     if f == "!done":
+        with open("output.md", "w") as file:
+            file.write(texts)
         exit()
     formatter = formatters.get(f)
     if formatter is None:
--- Markdown Editor/task/test/tests.py
+++ Markdown Editor/task/test/tests.py
@@ -3,6 +3,25 @@ from hstest import StageTest, TestedProgram, CheckResult, dynamic_test
 
 class Test(StageTest):
 
+    answers = [
+        '#### Hello World!\n',
+        'plain text**bold text**',
+        '*italic text*`code.work()`',
+        '[google](https://www.google.com)\n',
+        '1. first\n2. second\n3. third\n4. fourth\n'
+    ]
+
+    def check_result_in_file(self, attach):
+        try:
+            with open('output.md', 'r') as outfile:
+                output = outfile.read()
+                if output != self.answers[attach]:
+                    return CheckResult.wrong('The result written to the output file is wrong.')
+        except IOError:
+            return CheckResult.wrong('The output file is not found.')
+        return CheckResult.correct()
+
+
     @dynamic_test
     def test1(self):
         pr = TestedProgram()
@@ -11,18 +30,6 @@ class Test(StageTest):
         output = pr.execute('header').strip().lower()
         if 'level' not in output:
             return CheckResult.wrong('Header formatter should prompt a user for both level and text, i.e "- Level: > "')
-
-        ###########
-        input = '8'
-        output = list(map(lambda item: item.lower(), pr.execute(input).split('\n')))
-        expected_error_message = 'The level should be within the range of 1 to 6'
-
-        if len(output) != 2:
-            return CheckResult.wrong('A header formatter should output an error when the level is not between 1 and 6')
-
-        if expected_error_message.lower() not in output[0]:
-            return CheckResult.wrong('A header formatter should output an error when the level is not between 1 and 6')
-        ###########
 
         output = pr.execute('4').strip().lower()
         if 'text' not in output.strip().lower():
@@ -45,7 +52,7 @@ class Test(StageTest):
         if not pr.is_finished():
             return CheckResult.wrong('Your program should finish its execution whenever !done is an input')
 
-        return CheckResult.correct()
+        return self.check_result_in_file(attach=0)
 
     @dynamic_test
     def test2(self):
@@ -58,8 +65,7 @@ class Test(StageTest):
 
         output = list(map(lambda item: item.lower(), pr.execute('plain text').split('\n')))
         if len(output) != 2:
-            return CheckResult.wrong(
-                "Plain formatter should only return the given text as is, and prompt a user for a new formatter")
+            return CheckResult.wrong("Plain formatter should only return the given text as is, and prompt a user for a new formatter")
 
         if output[0] != 'plain text':
             return CheckResult.wrong('Plain formatter returns the given text as is, without any extra symbols or tags')
@@ -73,8 +79,7 @@ class Test(StageTest):
 
         output = list(map(lambda item: item.lower(), pr.execute('bold text').split('\n')))
         if len(output) != 2:
-            return CheckResult.wrong(
-                "Bold formatter should only return the given text enclosed with '**' symbols, and prompt a user for a new formatter")
+            return CheckResult.wrong("Bold formatter should only return the given text enclosed with '**' symbols, and prompt a user for a new formatter")
 
         if output[0] != 'plain text**bold text**':
             return CheckResult.wrong('Plain formatter returns the given text as is, and does not switch to a new line')
@@ -86,7 +91,7 @@ class Test(StageTest):
         if not pr.is_finished():
             return CheckResult.wrong('Your program should finish its execution whenever !done is an input')
 
-        return CheckResult.correct()
+        return self.check_result_in_file(attach=1)
 
     @dynamic_test
     def test3(self):
@@ -99,8 +104,7 @@ class Test(StageTest):
 
         output = list(map(lambda item: item.lower(), pr.execute('italic text').split('\n')))
         if len(output) != 2 or output[0] != '*italic text*':
-            return CheckResult.wrong(
-                "Bold formatter should only return the given text enclosed with '*' symbols, and prompt a user for a new formatter")
+            return CheckResult.wrong("Bold formatter should only return the given text enclosed with '*' symbols, and prompt a user for a new formatter")
 
         if 'formatter' not in output[1].strip():
             return CheckResult.wrong('A user should be prompted for input again, i.e  "- Choose a formatter: > "')
@@ -111,8 +115,7 @@ class Test(StageTest):
 
         output = list(map(lambda item: item.lower(), pr.execute('code.work()').split('\n')))
         if len(output) != 2:
-            return CheckResult.wrong(
-                "Inline code formatter should only return the given text enclosed with '`' (backtick) symbols, and prompt a user for a new formatter")
+            return CheckResult.wrong("Inline code formatter should only return the given text enclosed with '`' (backtick) symbols, and prompt a user for a new formatter")
 
         if output[0] != '*italic text*`code.work()`':
             return CheckResult.wrong('Inline code formatter does not switch to a new line')
@@ -124,7 +127,7 @@ class Test(StageTest):
         if not pr.is_finished():
             return CheckResult.wrong('Your program should finish its execution whenever !done is an input')
 
-        return CheckResult.correct()
+        return self.check_result_in_file(attach=2)
 
     @dynamic_test
     def test4(self):
@@ -141,20 +144,17 @@ class Test(StageTest):
 
         output = list(map(lambda item: item.lower(), pr.execute('https://www.google.com').split('\n')))
         if len(output) != 2:
-            return CheckResult.wrong(
-                'Link code formatter should only return the given label associated with a URL in the form [Label](URL), and prompt a user for a new formatter')
+            return CheckResult.wrong('Link code formatter should only return the given label associated with a URL in the form [Label](URL), and prompt a user for a new formatter')
 
         if output[0] != '[google](https://www.google.com)':
-            return CheckResult.wrong(
-                'Please recall that for the given label and URL the correct link formatter return will be [Label](URL)')
+            return CheckResult.wrong('Please recall that for the given label and URL the correct link formatter return will be [Label](URL)')
 
         if 'formatter' not in output[1].strip():
             return CheckResult.wrong('A user should be prompted for input again, i.e  "- Choose a formatter: > "')
 
         output = list(map(lambda item: item.lower(), pr.execute('new-line').split('\n')))
         if len(output) != 3 or output[1] != '':
-            return CheckResult.wrong(
-                'New-line formatter only moves the input pointer to the next line, and prompts a user for a new formatter')
+            return CheckResult.wrong('New-line formatter only moves the input pointer to the next line, and prompts a user for a new formatter')
 
         if output[0] != '[google](https://www.google.com)':
             return CheckResult.wrong('Please make sure that the markdown state is saved')
@@ -166,7 +166,40 @@ class Test(StageTest):
         if not pr.is_finished():
             return CheckResult.wrong('Your program should finish its execution whenever !done is an input')
 
-        return CheckResult.correct()
+        return self.check_result_in_file(attach=3)
+
+    @dynamic_test
+    def test5(self):
+        pr = TestedProgram()
+        pr.start()
+
+        output = pr.execute('ordered-list').strip().lower()
+        if 'number' not in output:
+            return CheckResult.wrong('Ordered list formatter should prompt a user for the number of rows, i.e "- Number of rows: > "')
+
+        output = list(map(lambda item: item.lower(), pr.execute('0').split('\n')))
+        if len(output) < 2 or 'number' not in output[-1].strip():
+            return CheckResult.wrong('(Un)ordered list formatter should inform a user that the number of rows should be greater than zero if the input was invalid, and prompt the user for this input again, i.e "- Number of rows: > "')
+
+        pr.execute('4')
+        pr.execute('first')
+        pr.execute('second')
+        pr.execute('third')
+        output = list(map(lambda item: item.lower(), pr.execute('fourth').split('\n')))
+        if len(output) != 6:
+            return CheckResult.wrong('Ordered list formatter should switch to a new line automatically')
+
+        if output[0] != '1. first' or output[1] != '2. second' or output[2] != '3. third' or output[3] != '4. fourth':
+            return CheckResult.wrong('Ordered list formatter should enumerate its rows in the following manner: "1. ", "2.", and so on, depending on the given number of rows.')
+
+        if 'formatter' not in output[5].strip():
+            return CheckResult.wrong('A user should be prompted for input again, i.e  "- Choose a formatter: > "')
+
+        pr.execute('!done')
+        if not pr.is_finished():
+            return CheckResult.wrong('Your program should finish its execution whenever !done is an input')
+
+        return self.check_result_in_file(attach=4)
 
 
 if __name__ == '__main__':
